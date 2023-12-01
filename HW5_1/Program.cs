42f8a8d062ae5e01db8b3a41c31160c60eddc627
Kukriniks <nad_antonio@tut.by>
2023-12-01 18:53:18 +0300

    HW_5 add

---
+++ HW5_1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HW5_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value;
+            int multiplicand;
+            Console.WriteLine( "input value");
+            while (!int.TryParse(Console.ReadLine(), out value))
+            {
+                Console.WriteLine("wrong value");
+            }
+            Console.WriteLine("input multiplicand");
+            
+            while (!int.TryParse(Console.ReadLine(), out multiplicand))
+            {
+                Console.WriteLine("wrong value");
+            }
+
+            for (int i = 1; i <= multiplicand; i++)
+            {
+                Console.WriteLine($"{value} x {i} = {value*i}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
