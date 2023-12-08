6840bbe0f8e66f42b16197aa6984968d4569ae7d
Kukriniks <nad_antonio@tut.by>
2023-12-08 13:58:11 +0300

    HW_7_task1 add try else)

---
+++ HW_7/HW_7_task1/ArayWorker.cs
@@ -0,0 +1,41 @@
+﻿namespace HW_7_task1
+{
+    public class ArrayWorker
+    {
+        private int[]? array;
+        public int[] Array
+        {
+            get
+            {
+                return array = array ?? System.Array.Empty<int>(); //avoid null reference exception                
+            }
+            set { array = value; }
+        }
+
+        public ArrayWorker(int[] array)
+        {
+            Console.WriteLine("Constructor with params");
+            for (int i = 0, j = array.Length - 1; i < array.Length / 2; i++, j--)
+            {
+                int tepmp = array[i];
+                array[i] = array[j];
+                array[j] = tepmp;
+            }
+            this.array = array;
+        }
+
+        public ArrayWorker()
+        { Console.WriteLine("Constructor withOut params"); }
+
+        public int[] InvertArray(int[] array)
+        {
+            for (int i = 0, j = array.Length - 1; i < array.Length / 2; i++, j--)
+            {
+                int tepmp = array[i];
+                array[i] = array[j];
+                array[j] = tepmp;
+            }
+            return array;
+        }
+    }
+}
---
+++ HW_7/HW_7_task1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace HW_7_task1
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+                int[] arrInit1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                int[] arrInit2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+                ArrayWorker arr1 = new ArrayWorker();
+                Console.WriteLine(arr1.Array);
+
+                foreach (var item in arr1.Array)
+                {
+                    Console.Write(item + " ");
+                }
+                foreach (var item in arr1.InvertArray(arrInit1))
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("next step");
+                ArrayWorker arr2 = new ArrayWorker(arrInit2);
+                foreach (var item in arr2.Array)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        
+    }
+}
