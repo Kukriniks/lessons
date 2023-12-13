cf5c09e09d3c02def7aa1660792376c3fcd42082
Kukriniks <nad_antonio@tut.by>
2023-12-13 06:32:30 +0300

    HW_8 task1

---
+++ HW_8/HW_8_ForTest_classes/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HW_8_ForTest_classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Item testGetName = new();
+            var name = testGetName.GetName();
+        }
+    }
+}
---
+++ HW_8/HW_8_Library/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HW_8_ForTest_classes
+{
+    public class Item
+    {
+        private string Name = "unknown";
+        public void SetName(string name) => this.Name = name;
+        //{ this.Name = name; }
+        public string GetName() => Name;
+        //{ return  Name; }
+    }
+}
