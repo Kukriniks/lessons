e0bea746f81ef6843f80f228885ed119ee0c624e
Kukriniks <nad_antonio@tut.by>
2024-01-12 22:33:38 +0300

    HW_14 task2 done

---
+++ TwoDigid/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace TwoDigid
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int count = 0;
+			int[] digit = new int[2];
+			while (count < 2)
+			{
+				Console.WriteLine($"input {count + 1} digit");
+				string? input = Console.ReadLine();
+
+				try
+				{
+					int dig = int.Parse(input);
+					if (dig >= 0 && dig < 255)
+					{
+						digit[count] = dig;
+						count++;
+					}
+					else
+						throw new ArgumentOutOfRangeException("0-255");
+
+				}
+				catch (FormatException ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+				catch (ArgumentOutOfRangeException ex) { Console.WriteLine(ex.Message); }
+			}
+
+			try
+			{
+				Console.WriteLine("try ti divide :)");
+				Console.WriteLine(digit[0] / digit[1]);
+			}
+			catch (DivideByZeroException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			catch (Exception ex)
+			{ Console.WriteLine(ex.Message); }
+		}
+	}
+}
