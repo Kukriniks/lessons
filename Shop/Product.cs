13865d40afde71c8fbe6bc19a9cfab9b70b64cb7
Kukriniks <nad_antonio@tut.by>
2024-01-12 13:04:13 +0300

    HW_14 start

---
+++ Shop/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    internal class Product : ISalable
+    {
+        public int Price { get ; set ; }
+        public string Name { get ; set; }
+
+        Product(int price, string name)
+        {
+            Price = price;
+            Name = name;
+        }
+    }
+}
