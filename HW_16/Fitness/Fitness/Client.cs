c143f0271ba82dcda1ad2a0db2cdc7541f5402d7
Kukriniks <nad_antonio@tut.by>
2024-01-20 19:55:39 +0300

    HW_16

---
+++ HW_16/Fitness/Fitness/Client.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.Collections;
+
+namespace Fitness
+{
+    internal class Client 
+    {
+        public Dictionary<int, Dictionary<int, int>> Year { get; set; }
+        public int ID { get; init; }
+
+        public Client(int ID)
+        {
+            Random rnd = new Random();
+            Year = new Dictionary<int, Dictionary<int, int>>();
+           
+            for (int j = 2010; j <= 2022; j++)
+            {
+                var temp = new Dictionary<int, int>();
+                for (int i = 0; i < 12; i++)
+                {
+                    temp.Add(i + 1, rnd.Next(0, 120)); //fit hours in a each month
+                }               
+                Year.Add(j, temp);
+            }
+
+            this.ID = ID;
+        }
+
+    }
+}
--- HW_16/Fitness/Fitness/Program.cs
+++ HW_16/Fitness/Fitness/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+          List<Client> clients = new List<Client>();
+
+            for (int i = 0; i < 120; i++)
+            {
+                clients.Add(new Client(i));                
+            }
+
+            //var test = clients.Select(p => p.Year).SelectMany(y => y.Values).ToList();
+            var test = (from p in clients
+                        from y in p.Year
+                        from m in y.Value
+                        group m by y.Key); //группируем по годам 
+                        
+            
+                       
         }
     }
 }
