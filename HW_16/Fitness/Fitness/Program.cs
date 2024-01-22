1ba71925c286ad79fecd1183c4c0a300213b03d4
Kukriniks <nad_antonio@tut.by>
2024-01-22 12:16:46 +0300

    Fitness task complete

--- HW_16/Fitness/Fitness/Program.cs
+++ HW_16/Fitness/Fitness/Program.cs
@@ -1,25 +1,31 @@
-﻿namespace Fitness
+﻿using Microsoft.VisualBasic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Fitness
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
-          List<Client> clients = new List<Client>();
+            List<Client> clients = new List<Client>();
 
             for (int i = 0; i < 120; i++)
             {
-                clients.Add(new Client(i));                
+                clients.Add(new Client(i));
             }
 
-            //var test = clients.Select(p => p.Year).SelectMany(y => y.Values).ToList();
             var test = (from p in clients
                         from y in p.Year
                         from m in y.Value
-                        group m by y.Key); //группируем по годам 
-                        
-            
-                       
+                        group m by y.Key into years  //группируем по годам  //select years;
+                        from m2 in years
+                        group m2.Value by years.Key into years2 //группируем месяцы // select years2;
+                        select new { Year = years2.Key, Month = years2.Aggregate((x, y) => x + y) }) //in value Month sum of fitness hours
+                       .OrderByDescending(x => x.Month).FirstOrDefault();
+            //так тут и не придумал , как получить несколько значений, если сумма часов в месяце вдруг окажется одинаковая ничегоне выходит (
+            Console.WriteLine($"In {test.Year} was {test.Month} total fitness hours");          
         }
     }
 }
+
