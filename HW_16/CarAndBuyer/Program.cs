82049f8c721623388eff845f959f3648215e548e
Kukriniks <nad_antonio@tut.by>
2024-01-23 10:11:39 +0300

    Car and Buyer  task complete

--- HW_16/CarAndBuyer/Program.cs
+++ HW_16/CarAndBuyer/Program.cs
@@ -7,7 +7,7 @@ namespace CarAndBuyer
         static void Main(string[] args)
         {
             List<Car> cars = new List<Car>();
-            List<Bueyer> buers = new List<Bueyer>();
+            List<Bueyer> buyer = new List<Bueyer>();
 
             cars.Add(new Car(id: 1, age: 3, carNamae: "Volvo")) ;
             cars.Add(new Car(id: 2, age: 5, carNamae: "BYD"));
@@ -15,43 +15,33 @@ namespace CarAndBuyer
             cars.Add(new Car(id: 4, age: 15, carNamae: "BMV"));
             cars.Add(new Car(id: 5, age: 7, carNamae: "Opel"));
 
-            buers.Add(new Bueyer(id: 1, carID: 1, "DIN-DON"));
-            buers.Add(new Bueyer(id: 1, carID: 5, "DIN-DON"));
+            buyer.Add(new Bueyer(id: 1, carID: 1, "DIN-DON"));
+            buyer.Add(new Bueyer(id: 1, carID: 5, "DIN-DON"));
 
-            buers.Add(new Bueyer(id: 2, carID: 4, "DAM_DAM"));
-            buers.Add(new Bueyer(id: 3, carID: 1, "PUM_PAM"));
+            buyer.Add(new Bueyer(id: 2, carID: 4, "DAM_DAM"));
+            buyer.Add(new Bueyer(id: 3, carID: 1, "PUM_PAM"));
 
-            var buersCars = (from b in buers
-                             group b by b.ID into buy
-                             from k in buy
-                             join c in cars on k.CarID equals c.ID
+            var buyrsCars = (from b in buyer
+                             join c in cars on b.CarID equals c.ID
                              select new
-                             {                                
-                                 Name = k.Name,
-                                 ID = k.ID,
-                                 carID = c.ID,
-                                 carName = c.Name
-                             }
-                             )
-                             .OrderBy(n => n.Name)
-                             .GroupBy(x => x.ID)
-                            ;
+                             {
+                                 BuyerName = b.Name,
+                                 BuyerId = b.ID,
+                                 CarId = c.ID,
+                                 CarName = c.Name,
+                                 CarAge = c.Age
+                             }).OrderBy(x => x.BuyerName)
+                             .GroupBy(x => x.BuyerId);
 
-
+            //Ничего красивее сделать так и не получилось.
+            
+            foreach (var buy in buyrsCars)
+            {
+                foreach (var item in buy)
+                {
+                    Console.WriteLine($"Buyer: {item.BuyerName} Car: {item.CarName}");
+                }
+            }
         }
-        //public class BuersCars
-        //{ 
-        //    public string Name { get; set; }
-        //    public List<int> carID { get; set; }
-        //    public List<string> carName { get; set;}
-        //    public int ID { get; set; }
-
-        //    public BuersCars(string Name, int[] CarID, string[] CarName, int ID)
-        //    {
-        //        this.carID = new List<int>();
-        //        this.carName = new List<string>();
-       
-        //    }
-        //}
     }
 }
