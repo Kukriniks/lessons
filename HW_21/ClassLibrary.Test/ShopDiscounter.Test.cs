4f0fd1d7673662d857586860b1c26ec71fb5f0e4
Kukriniks <nad_antonio@tut.by>
2024-02-09 16:42:49 +0300

    UnitTests

---
+++ HW_21/ClassLibrary.Test/ShopDiscounter.Test.cs
@@ -0,0 +1,52 @@
+﻿using ClassLibrary.Classes;
+using ClassLibrary.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Test
+{
+    [TestFixture]
+    internal class ShopDiscounterTests
+    {
+        private IShopDiscounter shopDiscounter;
+
+        [SetUp]
+        public void Setup()
+        { 
+            var logger = new Mock<ILogger>();
+           
+            logger.Setup(l => l.Warn(It.IsAny<string>()));
+            logger.Setup(l=>l.IsAvailable()).Returns(true);
+
+            shopDiscounter = new ShopDiscounter(logger.Object);
+        }
+
+        [TestCase (10)]
+        public void ShouldReturnItemWithCorrectPrice(int percent)
+        {            
+            var item = new ShopItem("test1", 100);
+
+            var result = shopDiscounter.AddDiscountToItem(item, percent);
+            Assert.That(result.Price, Is.EqualTo(90M));
+        }
+
+        [TestCase(101)]
+        public void ShouldTrowExceptionWhenPercentValueIncorrect(int percent)
+        {      
+            var item = new ShopItem("test1", 100);
+
+            var exception = Assert.Throws<Exception>(() =>
+            {
+                shopDiscounter.AddDiscountToItem(item, percent);
+            });
+
+            Assert.That(exception?.Message, Is.EqualTo("Wrong discount percent"));
+        }
+
+
+    }
+}
---
+++ HW_21/ClassLibrary.Test/ShopLoggerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+using ClassLibrary.Classes;
+using ClassLibrary.Interfaces;
+using Moq;
+
+namespace ClassLibrary.Test
+{
+    [TestFixture]
+    internal class ShopLoggerTests
+    {
+      
+        [Test]
+        public void ShopLoggerTest()
+        {
+            //Arrange
+            var databaseMoq = new Mock<IDb>();
+            databaseMoq.Setup( x => x.AddLogRecord(It.IsAny<string>()));
+            ShopLogger shoplogger = new ShopLogger(databaseMoq.Object);
+            string message = "test23";
+
+            //Act
+            shoplogger.Warn(message);
+
+            //Assert
+            databaseMoq.Verify(x => x.AddLogRecord($"WARN: {message}"));
+
+            //Act
+            shoplogger.Info(message);
+            //Assert
+            databaseMoq.Verify(x => x.AddLogRecord($"INFO: {message}"));
+
+        }
+
+
+    }
+}
+  
